5abd2fd471362826281d00e96860cbe1b2f026ad
Dimitar Shtregarski <mite_sht@abv.bg>
2025-01-23 11:18:53 +0200

    Intro to C++

---
+++ Term_01/Week_17_Intro_To_C++_22_01_2025/Solutions/helloWorld.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /// Принтираме на конзолата чрез оператора cout<<
+    cout<<"Hello, World!";
+    return 0;
+}
---
+++ Term_01/Week_17_Intro_To_C++_22_01_2025/Solutions/template.cpp
@@ -0,0 +1,9 @@
+/// Шаблон за всяка една програма на С++
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    return 0;
+}
